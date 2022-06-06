301537e20e1235a8e90e6a50a027eed605bbbaf8
Layne <Layne@atguigu.com>
2022-06-06 16:45:31 +0800

    second commit

---
+++ src/main/java/com/itlearn/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.itlearn.git;
+
+/**
+ * @Author Le'novo
+ * @Date 2022/6/6 16:41
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git1!!!");
+        System.out.println("Hello Git2!!!");
+    }
+}
